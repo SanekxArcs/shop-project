ce52c0808573cb74013de2ff04c5a483d332ee1e
Oleksandr Dzisiak <dzisiak@twnty.de>
2025-12-18 16:38:28 +0100

    feat: refactor Studio component and enhance ProjectCard styling

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,17 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from 'next'
 
 const nextConfig: NextConfig = {
+  compiler: {
+    styledComponents: true,
+  },
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "cdn.sanity.io",
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
       },
     ],
   },
-};
+}
 
-export default nextConfig;
+export default nextConfig
